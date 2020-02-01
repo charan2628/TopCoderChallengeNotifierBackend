9083f16adccbfa50849a18db8f2b38398987c30e
charan <s.charancherry22@gmail.com>
2020-02-01 18:06:09 +0530

    Added ErrorLog Service and Dao to asynchronously log messages to db

---
+++ src/main/java/com/app/dao/ErrorLogDao.java
@@ -0,0 +1,44 @@
+package com.app.dao;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+@Repository
+public class ErrorLogDao {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(MethodHandles.lookup().lookupClass());
+
+    private MongoCollection<String> collection;
+
+    public ErrorLogDao(
+            @Autowired MongoClient mongoClient, 
+            @Value("${app.db}") String databaseName,
+            @Value("${app.db.coll.errorlog}") String challenges) {
+        
+        this.collection = mongoClient.getDatabase(databaseName).getCollection(challenges, String.class);
+        LOGGER.debug("ErrorLogDao initialized successfully DB: {} COLLECTION: {}", databaseName, challenges);
+    }
+    
+    public void addErrorLog(String error) {
+        this.collection.insertOne(error);
+    }
+    
+    public List<String> getErrorLogs() {
+        List<String> errors = new ArrayList<>();
+        this.collection.find()
+            .forEach((Consumer<String>)(error) -> errors.add(error));
+        return errors;
+    }
+}
---
+++ src/main/java/com/app/service/ErrorLogService.java
@@ -0,0 +1,62 @@
+package com.app.service;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.app.dao.ErrorLogDao;
+
+/**
+ * Service class to add and get error logs
+ *
+ * @author charan2628
+ *
+ */
+@Service
+public class ErrorLogService {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(MethodHandles.lookup().lookupClass());
+
+    private ErrorLogDao errorLogDao;
+
+    public ErrorLogService(
+            @Autowired ErrorLogDao errorLogDao) {
+        this.errorLogDao = errorLogDao;
+    }
+
+    /**
+     * Get the errors logged in db.
+     *
+     * @return the list of error logs
+     */
+    public List<String> getErrorLogs() {
+        try {
+            return this.errorLogDao.getErrorLogs();
+        } catch (Exception e) {
+            LOGGER.error("Error getting error logs {}", e);
+            throw e;
+        }
+    }
+
+    /**
+     * Asynchronously adds log to db without
+     * holding of the client request in a separate thread pool
+     * configured in {@code AppConfig}.
+     *
+     * @param errorLog error log message
+     */
+    @Async("asnc_executor")
+    public void addErrorLog(String errorLog) {
+        try {
+            this.errorLogDao.addErrorLog(errorLog);
+        } catch (Exception e) {
+            LOGGER.error("Error adding log to db {}", e);
+        }
+    }
+}
